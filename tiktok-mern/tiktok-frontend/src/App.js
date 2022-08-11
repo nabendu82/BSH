e9078ad900b76571096b947f6ba09cf216cf4b14
nabendu82 <nabendu.biswas@gmail.com>
2022-08-11 12:03:31 +0530

    Day 10 start

---
+++ tiktok-mern/tiktok-frontend/src/App.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Video from './components/Video';
+import axios from './components/axios'
+
+function App() {
+  const [videos, setVideos] = useState([])
+
+  useEffect(() => {
+    const fetchData = async ()=> {
+      const res = await axios.get("/v2/posts")
+      setVideos(res.data)
+      return res
+    }
+    fetchData()
+  },[])
+
+  return (
+    <div className="app">
+      <div className="app__videos">
+        {videos.map(({ url, channel, description, song, likes, shares, messages}) =>
+          <Video 
+            url={url}
+            channel={channel}
+            description={description}
+            song={song}
+            likes={likes} 
+            shares={shares} 
+            messages={messages}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default App;
